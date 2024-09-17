f3ca657af1630e081b820c77e0a373c3a50ac1e8
nastialyash <user251@school22.rv.ua>
2024-09-17 18:52:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication67/ConsoleApplication67.cpp
@@ -0,0 +1,56 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, cals;
+    int startValue;
+
+    
+    cout << "Enter a cout of line: ";
+    cin >> rows;
+    cout << "Enter a cout of cols: ";
+    cin >> cals;
+
+    
+    cout << "Enter a start number : ";
+    cin >> startValue;
+
+    
+    int arr[1][1];
+
+  
+    arr[0][0] = startValue;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cals; j++) {
+            if (i == 0 && j == 0) {
+                continue; 
+            }
+            if (j == 0) {
+               
+                arr[i][j] = arr[i - 1][cals - 1] * 2;
+            }
+            else {
+              
+                arr[i][j] = arr[i][j - 1] * 2;
+            }
+        }
+    }
+
+    
+   
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cals; j++) {
+            cout << arr[i][j] <<endl;
+        }
+        cout << endl;
+    }
+
+
+
+
+
+
+}
+
